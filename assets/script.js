184efede803aeff3f05ce531376ec098d45733bc
Jennifer Ayala <j.ayala0695@gmail.com>
2021-07-30 16:57:52 -0700

    added to function

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,25 @@
 var key = "011270d41f698434cf9cd446d6dc399e";
 
-
-
-
-
 var location = document.getElementsByClassName('weather');
 
 function getApi(lat,lon) {
  var requestUrl = 'https://api.openweathermap.org/data/2.5/onecall?lat=' +lat+'&lon=' +lon+'&exclude=hourly,minutely,alerts&appid=' + key;   
   fetch(requestUrl)
     .then(function (response) {
-      console.log(location);
-      
+      console.log(location);  
       return response.json();
-  }) .then( function (data){
+  }) .then(function (data) {
     console.log(data);
+
+    document.getElementById('city').textContent = cityName.value;
+    document.getElementById('temp').textContent = data.current.temp;
+    document.getElementById('humidity').textContent = data.current.humidity;
+    document.getElementById('wind').textContent = data.current.wind_speed;
+    document.getElementById('uv-index').textContent = data.current.uv;
+
 })
+
+
 }
 
 function searchCity (cityName) {
